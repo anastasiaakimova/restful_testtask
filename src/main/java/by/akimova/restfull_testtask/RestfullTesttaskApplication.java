2a379de8cd4526b74ebe34e90f6e316270a195d8
Anastasiya Akimova <anastasiyaaki171@gmail.com>
2021-04-25 16:48:23 +0400

    fix some mistakes

--- src/main/java/by/akimova/restfull_testtask/RestfullTesttaskApplication.java
+++ src/main/java/by/akimova/restfull_testtask/RestfullTesttaskApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RestfullTesttaskApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(RestfullTesttaskApplication.class, args);
     }
 }
