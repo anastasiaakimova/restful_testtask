c4cbef583ccc24c6e13dc81cf0e059c77c2a7b2a
Anastasiya Akimova <anastasiyaaki171@gmail.com>
2021-04-25 16:12:44 +0400

    modified service and serviceimpl

--- src/main/java/by/akimova/restfull_testtask/services/UserService.java
+++ src/main/java/by/akimova/restfull_testtask/services/UserService.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserService {
-    Optional<User> getById(Long id);
+    User getById(Long id);
     void save(User user);
     void delete(Long id);
     List<User> getAll();
--- src/main/java/by/akimova/restfull_testtask/services/impl/UserServiceImpl.java
+++ src/main/java/by/akimova/restfull_testtask/services/impl/UserServiceImpl.java
@@ -1,32 +1,35 @@
-package by.akimova.restfull_testtask.services;
+package by.akimova.restfull_testtask.services.impl;
 
 import by.akimova.restfull_testtask.models.User;
 import by.akimova.restfull_testtask.repository.UserRepo;
+import by.akimova.restfull_testtask.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class UserServiceImpl implements UserService{
-    @Autowired
-    UserRepo userRepo;
+public class UserServiceImpl implements UserService {
+   private final UserRepo userRepo;
+
+   @Autowired
+    public UserServiceImpl(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
 
     @Override
     public User getById(Long id) {
-        return userRepo.findOne(id);
+        return userRepo.getOne(id);
     }
 
     @Override
     public void save(User user) {
         userRepo.save(user);
-
     }
 
     @Override
     public void delete(Long id) {
-        userRepo.delete(id);
-
+        userRepo.deleteById(id);
     }
 
     @Override
