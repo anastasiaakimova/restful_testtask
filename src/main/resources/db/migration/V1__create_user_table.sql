e7de686d901a84a98825ea1ee667c49a601b7974
Anastasiya Akimova <anastasiyaaki171@gmail.com>
2021-04-25 23:15:14 +0400

    modified table

--- src/main/resources/db/migration/V1__create_user_table.sql
+++ src/main/resources/db/migration/V1__create_user_table.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS user
+CREATE TABLE IF NOT EXISTS usr
 (   id SERIAL PRIMARY KEY,
     password character varying(255) NOT NULL,
     username character varying(255) NOT NULL,
