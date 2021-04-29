c0b68d89a6bc11e1a05057468ac86763ed4db79f
Anastasiya Akimova <anastasiyaaki171@gmail.com>
2021-04-30 00:16:39 +0400

    add enam component body

---
+++ src/main/java/by/akimova/restfull_testtask/models/Body.java
@@ -0,0 +1,15 @@
+package by.akimova.restfull_testtask.models;
+
+public enum Body {
+    // внедорожник
+    SUV,
+    CABRIOLET,
+    COUPE,
+    LIFTBACK,
+    MINIVAN,
+    PICKUP,
+    SEDAN,
+    UNIVERSAL,
+    VAN,
+    HATCHBACK;
+}
